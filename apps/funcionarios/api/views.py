7cfadb45355ab4bd301a134cadcd3c18ef5e034b
Suelio Santos <sueliosantos@gmail.com>
2022-02-23 22:52:37 -0300

    Usando token na api de funcionario

--- apps/funcionarios/api/views.py
+++ apps/funcionarios/api/views.py
@@ -3,9 +3,12 @@ from rest_framework import viewsets
 from rest_framework import permissions
 from apps.funcionarios.api.serializers import FuncionarioSerializer
 from apps.funcionarios.models import Funcionario
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
 
 
 class FuncionarioViewSet(viewsets.ModelViewSet):
     queryset = Funcionario.objects.all()
     serializer_class = FuncionarioSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
